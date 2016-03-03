c6662e953d6e3d7fa0969ed6b4f78370c76c07d4
Shane Dharan <ShaneDharan@gmail.com>
2016-03-03 07:15:05 -0500

    Starting List

--- WeatherApplication/app/src/main/java/shanedharan/me/weatherapplication/MainActivity.java
+++ WeatherApplication/app/src/main/java/shanedharan/me/weatherapplication/MainActivity.java
@@ -1,14 +1,62 @@
 package shanedharan.me.weatherapplication;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private int[] tempValues;
+    private SensorManager tempManager;
+    private Sensor ambientTemp;
+    private Temperature serverTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initalize the title to show ambient temp.
         setTitle("Current Temperature: ");
+
+        tempValues = new int[5];
+        serverTemp = new Temperature();
+
+        setup();
+    }
+
+    public void setup() {
+        //0th position is made for ambient temp.
+        tempValues[0] = 0;
+
+        //Get values from "Server"
+        for(int i = 1; i < 5; i++) {
+            tempValues[i] = serverTemp.getValue();
+        }
+
+        //Get instance of the sensor service
+        tempManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(tempManager.getSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE).size() != 0) {
+            ambientTemp = tempManager.getSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE).get(0);
+            tempManager.registerListener(this, ambientTemp, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            //no temperature sensor available
+            setTitle("No Temperature Sensor Available");
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //if value changes, change the title...
+        float temp = event.values[0];
+        setTitle("Current Temperature: " + temp);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
---
+++ WeatherApplication/app/src/main/java/shanedharan/me/weatherapplication/Temperature.java
@@ -0,0 +1,26 @@
+package shanedharan.me.weatherapplication;
+
+import java.util.Random;
+
+/**
+ * Created by shane on 03/03/16.
+ *
+ * Currently this class only generates random numbers, however if this were a real weather
+ * application I would use this class for server communication to get the next 5 day forecast.
+ */
+public class Temperature {
+
+    //Canada's temperature sucks
+    private static final int MIN_TEMP = -14;
+    private static final int MAX_TEMP = 9;
+
+    private Random tempValue;
+
+    public Temperature() {
+        tempValue = new Random();
+    }
+
+    public int getValue() {
+        return tempValue.nextInt(MAX_TEMP - MIN_TEMP) + MIN_TEMP;
+    }
+}
